e1f3f144a76b9370e0496069de1e9bd7ecea49cf
JatinNarendra <jatinkumarghetiya026@gmail.com>
2022-06-11 20:20:05 +0530

    uploading files

---
+++ src/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({robots}) => 
+{
+	return(
+				<div>
+					{
+						robots.map((user,i) => 
+							{	
+								return (
+									<Card key= {i} id={robots[i].id} name={robots[i].name} email={robots[i].email}
+								/>
+							);
+						})
+					}		
+				</div>
+		);
+}
+export default CardList;
+
+// <div>
+//                     
+//                     <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+//                     <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+//                   </div>
