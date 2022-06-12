b81624b3f181d6064b199413925774361becfa79
JatinNarendra <jatinkumarghetiya026@gmail.com>
2022-06-12 12:47:53 +0530

    Updates

---
+++ precache-manifest.d0a32c9f9b73652e452eca216102645e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "929cf93e0d8fa51d16c0",
+    "url": "/robofriends/static/css/main.4420eeb4.chunk.css"
+  },
+  {
+    "revision": "929cf93e0d8fa51d16c0",
+    "url": "/robofriends/static/js/main.929cf93e.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robofriends/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "bd2b17d0cff73908e499",
+    "url": "/robofriends/static/css/2.c89819c6.chunk.css"
+  },
+  {
+    "revision": "bd2b17d0cff73908e499",
+    "url": "/robofriends/static/js/2.bd2b17d0.chunk.js"
+  },
+  {
+    "revision": "5a6fc926011dbcb5a003",
+    "url": "/robofriends/static/js/runtime~main.5a6fc926.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/robofriends/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "632ce01858da59f791f5ffecef910d60",
+    "url": "/robofriends/index.html"
+  }
+];
